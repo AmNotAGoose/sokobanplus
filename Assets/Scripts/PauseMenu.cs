45ad12810db2ad3a85585b4cd104738a8c2ab6a4
AmNotAGoose <57881736+AmNotAGoose@users.noreply.github.com>
2025-11-16 08:31:27 -0500

    actually fix

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 [System.Serializable]
@@ -16,10 +17,30 @@ public class PauseMenu : MonoBehaviour
     public GameObject currentMenu;
     public List<PauseMenus> pauseMenus = new();
 
+    public GameObject menuObject;
+
+
+    public void TogglePauseGame()
+    {
+        menuObject.SetActive(!menuObject.activeSelf);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseGame();
+        }
+    }
+
     public void SwitchMenu(string id)
     {
         currentMenu.SetActive(false);
         currentMenu = pauseMenus.Find(menu => menu.id == id).menu;
         currentMenu.SetActive(true);
+    }
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,34 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject pauseMenu;
-    public bool canPause = false;
-
-    private void Start()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    public void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
-    {
-        canPause = !(scene.name == "Menu");
-
-        if (!canPause) pauseMenu.SetActive(false);
-    }
-    public void TogglePauseGame()
-    {
-        if (!canPause) return;
-        pauseMenu.SetActive(!pauseMenu.activeSelf);
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            TogglePauseGame();
-        }
-    }
-
     public void LoadScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
